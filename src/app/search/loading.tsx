1edec1a3c2259ac142f1a4ab82225af1ade17394
Ramiz Ahmed <ramizahmedar@gmail.com>
2023-04-26 03:06:00 +0500

    Add loader for search

--- src/app/search/loading.tsx
+++ src/app/search/loading.tsx
@@ -1,3 +1,20 @@
+import Header from "@/components/Header";
+
 export default function loading() {
-  return <div className="bg-orange-300 h-screen">loadinggggggggg...</div>;
+  return (
+    <>
+      <Header />
+      <div className="flex py-4 m-auto w-2/3 justify-between items-start">
+        <div className="w-1/5 animate-pulse bg-slate-200 h-96 rounded m-3 cursor-pointer"></div>
+        <div className="w-5/6">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => (
+            <div
+              key={num}
+              className="border-b flex pb-5 animate-pulse bg-slate-200 w-[825px] h-40 rounded m-3 cursor-pointer"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
