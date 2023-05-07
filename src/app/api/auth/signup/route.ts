3e36ae98a70de50083ae0f9d28ce07f8bdd65f35
Ramiz Ahmed <ramizahmedar@gmail.com>
2023-05-08 03:18:02 +0500

    Add sign up endpoint

---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,83 @@
+import { NextRequest, NextResponse } from "next/server";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import * as jose from "jose";
+
+import prisma from "@/lib/prisma";
+
+export async function POST(req: NextRequest) {
+  const { firstName, lastName, city, password, email, phone } =
+    await req.json();
+  const errors: string[] = [];
+  const validationSchema = [
+    {
+      valid: validator.isLength(firstName, {
+        min: 1,
+        max: 20,
+      }),
+      errorMessage: "First name is invalid",
+    },
+    {
+      valid: validator.isLength(lastName, {
+        min: 1,
+        max: 20,
+      }),
+      errorMessage: "Last name is invalid",
+    },
+    {
+      valid: validator.isEmail(email),
+      errorMessage: "Email is invalid",
+    },
+    {
+      valid: validator.isMobilePhone(phone),
+      errorMessage: "Phone number is invalid",
+    },
+    {
+      valid: validator.isLength(city, { min: 1 }),
+      errorMessage: "City is invalid",
+    },
+    {
+      valid: validator.isStrongPassword(password),
+      errorMessage: "Password is invalid",
+    },
+  ];
+  validationSchema.forEach((check) => {
+    if (!check.valid) {
+      errors.push(check.errorMessage);
+    }
+  });
+  if (errors.length) return NextResponse.json({ errorMessage: errors[0] });
+
+  const userExists = await prisma.user.findUnique({
+    where: { email },
+    select: { id: true },
+  });
+  if (userExists)
+    return NextResponse.json(
+      { errorMessage: "Email is already taken" },
+      { status: 400 }
+    );
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await prisma.user.create({
+    data: {
+      first_name: firstName,
+      last_name: lastName,
+      city,
+      email,
+      phone,
+      password: hashedPassword,
+    },
+  });
+  const alg = "HS256";
+  const secret = new TextEncoder().encode(process.env.JWT_SECRET);
+  const token = await new jose.SignJWT({ email: user.email })
+    .setProtectedHeader({ alg })
+    .setExpirationTime("24h")
+    .sign(secret);
+
+  return NextResponse.json({
+    token,
+  });
+}
